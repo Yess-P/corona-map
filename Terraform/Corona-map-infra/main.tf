2202905b309d2712614e0a3d23ce5c7df4bad627
Yess-P <79623220+Yess-P@users.noreply.github.com>
2021-07-14 22:38:38 +0900

    Update main.tf

--- Terraform/Corona-map-infra/main.tf
+++ Terraform/Corona-map-infra/main.tf
@@ -473,3 +473,16 @@ resource "aws_instance" "EKS-Jenkins-Slave"{
         "Name" = "EKS-Jenkins-Slave"
         }
 }
+  
+resource "aws_db_instance" "mysql" {
+  allocated_storage     = 10
+  max_allocated_storage = 20
+  engine                = "mysql"
+  engine_version        = "5.7"
+  instance_class        = "db.t3.micro"
+  name                  = "mydb"
+  username              = "foo"
+  password              = "foobarbaz"
+  parameter_group_name  = "default.mysql5.7"
+  skip_final_snapshot   = true
+}
